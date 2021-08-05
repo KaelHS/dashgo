8c724c615353bc2fb5c0587eb98aef57787fcdcb
Kael Silva <kael_hs@hotmail.com>
2021-08-05 11:47:42 -0300

    pagination logic

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,41 +4,16 @@ import Link from 'next/link';
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
-import { useEffect } from "react";
-import { useQuery } from 'react-query';
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
 
-    const { data, isLoading, error } = useQuery('users', async () => {
-
-        const response = await fetch('http://localhost:3000/api/users');
-        
-        const data = await response.json();
-
-        const users = data.users.map( user => ({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric'
-            })
-
-        }))
-
-        return users;
-    }, {
-        staleTime: 5000,
-    })
+    const { data, isLoading, isFetching, error } = useUsers();
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg:true
     });
-
-    useEffect( () => {
-    }, []);
 
     return(
         <Box>
@@ -48,7 +23,10 @@ export default function UserList() {
                 <Sidebar />
             <Box flex="1" borderRadius="8" bg="gray.800" p="8">
                 <Flex mb="8" justify="space-between" align="center">
-                    <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                    <Heading size="lg" fontWeight="normal">
+                        Usuários
+                        { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+                    </Heading>
 
                     <Link href="/users/create" passHref>
                         <Button 
